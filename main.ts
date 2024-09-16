eda3c31695c485b7eeb18adc7a5e911fa202d973
Breawn Schoun <breawn.schoun@ucdenver.edu>
2024-09-16 11:25:36 -0600

    Names sprites/animations, reduced redundancy

--- main.ts
+++ main.ts
@@ -10,52 +10,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
     animation.runImageAnimation(
     mySprite,
-    [img`
-        . . 4 4 4 . . . . 4 4 4 . . . . 
-        . 4 5 5 5 e . . e 5 5 5 4 . . . 
-        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-        . e e 5 5 5 5 5 5 5 5 e e . . . 
-        . . e 5 f 5 5 5 5 f 5 e . . . . 
-        . . f 5 5 5 4 4 5 5 5 f . . f f 
-        . . f 4 5 5 f f 5 5 6 f . f 5 f 
-        . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
-        . . . f 4 5 5 5 5 5 5 4 4 5 f . 
-        . . . f 5 5 5 5 5 4 5 5 f f . . 
-        . . . f 5 f f f 5 f f 5 f . . . 
-        . . . f f . . f f . . f f . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . 4 4 4 . . . . 4 4 4 . . . . 
-        . 4 5 5 5 e . . e 5 5 5 4 . . . 
-        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-        . e e 5 5 5 5 5 5 5 5 e e . . . 
-        . . e 5 f 5 5 5 5 f 5 e . . . . 
-        . . f 5 5 5 4 4 5 5 5 f . f f . 
-        . . . 4 5 5 f f 5 5 6 f f 5 f . 
-        . . . f 6 6 6 6 6 6 4 4 4 5 f . 
-        . . . f 5 5 5 5 5 5 5 f f f . . 
-        . . . f 5 4 5 f f f 5 f . . . . 
-        . . . f f f f f . . f f . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . 4 4 4 . . . . 4 4 4 . . . . 
-        . 4 5 5 5 e . . e 5 5 5 4 . . . 
-        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
-        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
-        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
-        . e e 5 5 5 5 5 5 5 5 e e . . . 
-        . . e 5 f 5 5 5 5 f 5 e . . . . 
-        . . f 5 5 5 4 4 5 5 5 f . f f . 
-        . . . 4 5 5 f f 5 5 6 f f 5 f . 
-        . . . f 6 6 6 6 6 6 4 f 5 5 f . 
-        . . . f 5 5 5 5 5 5 5 4 5 f . . 
-        . . . . f 5 4 5 f 5 f f f . . . 
-        . . . . . f f f f f f f . . . . 
-        `],
+    assets.animation`DogRunLeft`,
     100,
     true
     )
@@ -66,92 +21,17 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`tile3`, function (sprite, loc
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
-    mySprite.setImage(img`
-        . . . . 4 4 4 . . . . 4 4 4 . . 
-        . . . 4 5 5 5 e . . e 5 5 5 4 . 
-        . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-        . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-        . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-        . . . e e 5 5 5 5 5 5 5 5 e e . 
-        . . . . e 5 f 5 5 5 5 f 5 e . . 
-        f f . . f 5 5 5 4 4 5 5 5 f . . 
-        f 5 f . f 6 5 5 f f 5 5 4 f . . 
-        f 5 5 f 4 4 6 6 6 6 6 6 f . . . 
-        . f 5 4 4 5 5 5 5 5 5 4 f . . . 
-        . . f f 5 5 4 5 5 5 5 5 f . . . 
-        . . . f 5 f f 5 f f f 5 f . . . 
-        . . . f f . . f f . . f f . . . 
-        `)
+    mySprite.setImage(assets.image`DogRight`)
 })
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
-    mySprite.setImage(img`
-        . . . . 4 4 4 . . . . 4 4 4 . . 
-        . . . 4 5 5 5 e . . e 5 5 5 4 . 
-        . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-        . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-        . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-        . . . e e 5 5 5 5 5 5 5 5 e e . 
-        . . . . e 5 f 5 5 5 5 f 5 e . . 
-        f f . . f 5 5 5 4 4 5 5 5 f . . 
-        f 5 f . f 6 5 5 f f 5 5 4 f . . 
-        f 5 5 f 4 4 6 6 6 6 6 6 f . . . 
-        . f 5 4 4 5 5 5 5 5 5 4 f . . . 
-        . . f f 5 5 4 5 5 5 5 5 f . . . 
-        . . . f 5 f f 5 f f f 5 f . . . 
-        . . . f f . . f f . . f f . . . 
-        `)
+    mySprite.setImage(assets.image`DogRight`)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.stopAnimation(animation.AnimationTypes.All, mySprite)
     animation.runImageAnimation(
     mySprite,
-    [img`
-        . . . . 4 4 4 . . . . 4 4 4 . . 
-        . . . 4 5 5 5 e . . e 5 5 5 4 . 
-        . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-        . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-        . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-        . . . e e 5 5 5 5 5 5 5 5 e e . 
-        . . . . e 5 f 5 5 5 5 f 5 e . . 
-        f f . . f 5 5 5 4 4 5 5 5 f . . 
-        f 5 f . f 6 5 5 f f 5 5 4 f . . 
-        f 5 5 f 4 4 6 6 6 6 6 6 f . . . 
-        . f 5 4 4 5 5 5 5 5 5 4 f . . . 
-        . . f f 5 5 4 5 5 5 5 5 f . . . 
-        . . . f 5 f f 5 f f f 5 f . . . 
-        . . . f f . . f f . . f f . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . 4 4 4 . . . . 4 4 4 . . 
-        . . . 4 5 5 5 e . . e 5 5 5 4 . 
-        . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-        . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-        . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-        . . . e e 5 5 5 5 5 5 5 5 e e . 
-        . . . . e 5 f 5 5 5 5 f 5 e . . 
-        . f f . f 5 5 5 4 4 5 5 5 f . . 
-        . f 5 f f 6 5 5 f f 5 5 4 . . . 
-        . f 5 4 4 4 6 6 6 6 6 6 f . . . 
-        . . f f f 5 5 5 5 5 5 5 f . . . 
-        . . . . f 5 f f f 5 4 5 f . . . 
-        . . . . f f . . f f f f f . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . 4 4 4 . . . . 4 4 4 . . 
-        . . . 4 5 5 5 e . . e 5 5 5 4 . 
-        . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-        . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-        . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-        . . . e e 5 5 5 5 5 5 5 5 e e . 
-        . . . . e 5 f 5 5 5 5 f 5 e . . 
-        . f f . f 5 5 5 4 4 5 5 5 f . . 
-        . f 5 f f 6 5 5 f f 5 5 4 . . . 
-        . f 5 5 f 4 6 6 6 6 6 6 f . . . 
-        . . f 5 4 5 5 5 5 5 5 5 f . . . 
-        . . . f f f 5 f 5 4 5 f . . . . 
-        . . . . f f f f f f f . . . . . 
-        `],
+    assets.animation`RunRight`,
     100,
     true
     )
@@ -159,22 +39,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(9)
-mySprite = sprites.create(img`
-    . . . . 4 4 4 . . . . 4 4 4 . . 
-    . . . 4 5 5 5 e . . e 5 5 5 4 . 
-    . . 4 5 5 5 5 5 e e 5 5 5 5 5 4 
-    . . 4 5 5 4 4 5 5 5 5 4 4 5 5 4 
-    . . e 5 4 4 5 5 5 5 5 5 4 4 5 e 
-    . . . e e 5 5 5 5 5 5 5 5 e e . 
-    . . . . e 5 f 5 5 5 5 f 5 e . . 
-    f f . . f 5 5 5 4 4 5 5 5 f . . 
-    f 5 f . f 6 5 5 f f 5 5 4 f . . 
-    f 5 5 f 4 4 6 6 6 6 6 6 f . . . 
-    . f 5 4 4 5 5 5 5 5 5 4 f . . . 
-    . . f f 5 5 4 5 5 5 5 5 f . . . 
-    . . . f 5 f f 5 f f f 5 f . . . 
-    . . . f f . . f f . . f f . . . 
-    `, SpriteKind.Player)
+mySprite = sprites.create(assets.image`DogRight`, SpriteKind.Player)
 mySprite.ay = 200
 tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 12))
 scene.cameraFollowSprite(mySprite)
